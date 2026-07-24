f6c21e1aded736244b55efd2ec1a76fcc6d5eb1b
Runteryaa <runteryaa@github.com>
2026-07-24 11:32:16 +0300

    Fix Reanimated New Architecture Conflict

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import TrackPlayer, { Capability } from 'react-native-track-player';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootNavigator from './src/navigation/RootNavigator';
 
 const queryClient = new QueryClient();
@@ -32,13 +31,11 @@ export default function App() {
   }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <RootNavigator />
-          <StatusBar style="auto" />
-        </NavigationContainer>
-      </QueryClientProvider>
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
